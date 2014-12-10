c32adf0133555e678f0660a33874af67bb6ae5c2
Anamonus <das316@hotmail.com>
2014-12-10 11:59:06 -0800

    Overloads Done

--- MathLibrary/MathStuff.cpp
+++ MathLibrary/MathStuff.cpp
@@ -9,3 +9,16 @@ PosTracker::PosTracker()
 PosTracker::~PosTracker()
 {
 }
+void PosTracker::createMatrix3x3(float a_x, float a_y)
+{
+	firstmtrx[0][0] = a_x;
+	firstmtrx[0][1] = 0;
+	firstmtrx[0][2] = 0;
+	firstmtrx[1][0] = 0;
+	firstmtrx[1][1] = a_y;
+	firstmtrx[1][2] = 0;
+	firstmtrx[2][0] = 0;
+	firstmtrx[2][1] = 0;
+	firstmtrx[2][2] = 1;
+
+}
--- MathLibrary/MathStuff.h
+++ MathLibrary/MathStuff.h
@@ -9,6 +9,7 @@ public:
 	int firstmtrx[3][3];
 	int secondmtrx[3][3];
 	int resultmtrx[3][3];
+	void createMatrix3x3(float a_x, float a_y);
 	PosTracker operator=(PosTracker &rhs)
 	{
 		int temp;
@@ -74,6 +75,116 @@ public:
 		resultmtrx[2][2] = temp;
 	}
 
+	PosTracker operator+(PosTracker &rhs)
+	{
+		int temp;
+		temp = firstmtrx[0][0] + secondmtrx[0][0];
+		resultmtrx[0][0] = temp;
+		temp = firstmtrx[0][1] + secondmtrx[0][1];
+		resultmtrx[0][1] = temp;
+		temp = firstmtrx[0][2] + secondmtrx[0][2];
+		resultmtrx[0][2] = temp;
+		temp = firstmtrx[1][0] + secondmtrx[1][0];
+		resultmtrx[1][0] = temp;
+		temp = firstmtrx[1][1] + secondmtrx[1][1];
+		resultmtrx[1][1] = temp;
+		temp = firstmtrx[1][2] + secondmtrx[1][2];
+		resultmtrx[1][2] = temp;
+		temp = firstmtrx[2][0] + secondmtrx[2][0];
+		resultmtrx[2][0] = temp;
+		temp = firstmtrx[2][1] + secondmtrx[2][1];
+		resultmtrx[2][1] = temp;
+		temp = firstmtrx[2][2] + secondmtrx[2][2];
+		resultmtrx[2][2] = temp;
+	}
+	PosTracker operator-(PosTracker &rhs)
+	{
+		int temp;
+		temp = firstmtrx[0][0] - secondmtrx[0][0];
+		resultmtrx[0][0] = temp;
+		temp = firstmtrx[0][1] - secondmtrx[0][1];
+		resultmtrx[0][1] = temp;
+		temp = firstmtrx[0][2] - secondmtrx[0][2];
+		resultmtrx[0][2] = temp;
+		temp = firstmtrx[1][0] - secondmtrx[1][0];
+		resultmtrx[1][0] = temp;
+		temp = firstmtrx[1][1] - secondmtrx[1][1];
+		resultmtrx[1][1] = temp;
+		temp = firstmtrx[1][2] - secondmtrx[1][2];
+		resultmtrx[1][2] = temp;
+		temp = firstmtrx[2][0] - secondmtrx[2][0];
+		resultmtrx[2][0] = temp;
+		temp = firstmtrx[2][1] - secondmtrx[2][1];
+		resultmtrx[2][1] = temp;
+		temp = firstmtrx[2][2] - secondmtrx[2][2];
+		resultmtrx[2][2] = temp;
+	}
+	PosTracker operator+=(PosTracker &rhs)
+	{
+		int temp;
+		temp = firstmtrx[0][0] + firstmtrx[0][0];
+		resultmtrx[0][0] = temp;
+		temp = firstmtrx[0][1] + firstmtrx[0][1];
+		resultmtrx[0][1] = temp;
+		temp = firstmtrx[0][2] + firstmtrx[0][2];
+		resultmtrx[0][2] = temp;
+		temp = firstmtrx[1][0] + firstmtrx[1][0];
+		resultmtrx[1][0] = temp;
+		temp = firstmtrx[1][1] + firstmtrx[1][1];
+		resultmtrx[1][1] = temp;
+		temp = firstmtrx[1][2] + firstmtrx[1][2];
+		resultmtrx[1][2] = temp;
+		temp = firstmtrx[2][0] + firstmtrx[2][0];
+		resultmtrx[2][0] = temp;
+		temp = firstmtrx[2][1] + firstmtrx[2][1];
+		resultmtrx[2][1] = temp;
+		temp = firstmtrx[2][2] + firstmtrx[2][2];
+		resultmtrx[2][2] = temp;
+	}
+	PosTracker operator-=(PosTracker &rhs)
+	{
+		int temp;
+		temp = firstmtrx[0][0] - firstmtrx[0][0];
+		resultmtrx[0][0] = temp;
+		temp = firstmtrx[0][1] - firstmtrx[0][1];
+		resultmtrx[0][1] = temp;
+		temp = firstmtrx[0][2] - firstmtrx[0][2];
+		resultmtrx[0][2] = temp;
+		temp = firstmtrx[1][0] - firstmtrx[1][0];
+		resultmtrx[1][0] = temp;
+		temp = firstmtrx[1][1] - firstmtrx[1][1];
+		resultmtrx[1][1] = temp;
+		temp = firstmtrx[1][2] - firstmtrx[1][2];
+		resultmtrx[1][2] = temp;
+		temp = firstmtrx[2][0] - firstmtrx[2][0];
+		resultmtrx[2][0] = temp;
+		temp = firstmtrx[2][1] - firstmtrx[2][1];
+		resultmtrx[2][1] = temp;
+		temp = firstmtrx[2][2] - firstmtrx[2][2];
+		resultmtrx[2][2] = temp;
+	}
+	PosTracker operator==(PosTracker &rhs)
+	{
+		int temp;
+		temp = firstmtrx[0][0] = secondmtrx[0][0];
+		resultmtrx[0][0] = temp;
+		temp = firstmtrx[0][1] = secondmtrx[0][1];
+		resultmtrx[0][1] = temp;
+		temp = firstmtrx[0][2] = secondmtrx[0][2];
+		resultmtrx[0][2] = temp;
+		temp = firstmtrx[1][0] = secondmtrx[1][0];
+		resultmtrx[1][0] = temp;
+		temp = firstmtrx[1][1] = secondmtrx[1][1];
+		resultmtrx[1][1] = temp;
+		temp = firstmtrx[1][2] = secondmtrx[1][2];
+		resultmtrx[1][2] = temp;
+		temp = firstmtrx[2][0] = secondmtrx[2][0];
+		resultmtrx[2][0] = temp;
+		temp = firstmtrx[2][1] = secondmtrx[2][1];
+		resultmtrx[2][1] = temp;
+		temp = firstmtrx[2][2] = secondmtrx[2][2];
+		resultmtrx[2][2] = temp;
+	}
 private:
 
 };
